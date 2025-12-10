d54dd80aca35909c1ea63f8fdeffa303a4f55fce
Kamil <k.mirgasimov@innopolis.university>
2025-12-11 00:55:02 +0300

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://178.72.139.21"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
